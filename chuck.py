4c9ba5c0dd8deee4d31de02ccba70f1ff3ee9cba
Enrico <rico.bl@gmail.com>
2010-10-28 13:26:11 -0200

    Added basic assertions.

---
+++ chuck.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def assert_true(value, msg=None):
+    return True
+
+def assert_false(value, msg=None):
+    return True
+
+def assert_raises(excClass, callableObj, *args, **kwargs):
+    return True
+
+def assert_equals(first, second, msg=None):
+    return True
+
+def assert_not_equals(first, second, msg=None):
+    return True
+

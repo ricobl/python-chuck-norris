99de973f705c815be2f223c57f04762b27ae7280
Enrico <rico.bl@gmail.com>
2010-10-28 22:39:02 -0200

    Refactored tests and added fail method.

--- chuck.py
+++ chuck.py
@@ -4,21 +4,29 @@
 __version__ = '0.1'
 
 
+class RoundHouseKick(Exception):
+    pass
+
+
+def fail():
+    raise RoundHouseKick('Fix your test. Chuck Norris never fails.')
+
+
 def assert_true(value, msg=None):
-    return True
+    pass
 
 
 def assert_false(value, msg=None):
-    return True
+    pass
 
 
 def assert_raises(excClass, callableObj, *args, **kwargs):
-    return True
+    pass
 
 
 def assert_equals(first, second, msg=None):
-    return True
+    pass
 
 
 def assert_not_equals(first, second, msg=None):
-    return True
+    pass
